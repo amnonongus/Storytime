f981a8d1f251376f97cc5aae8de6b9e506ff6280
amnonongus <charmilano@gmail.com>
2022-04-13 14:21:52 -0700

    cleaning up the code looks better

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,27 +1,20 @@
 const Story = require('../models/story')
 var router = require('express').Router();
 
-//router.put for editing specific comment that you've entered
-
-//router.delete for deleting a comment
-
-//router.post adding comment to specific story
-
-
-
 
 router.post('/stories/:id', function(req, res){
-    console.log('helllo post route')
+  console.log('helllo post route')
     Story.findById(req.params.id, function(err, storyFromDatabase) {
-        req.body.userID = req.user._id;
-        console.log(storyFromDatabase)
-         storyFromDatabase.content.push(req.body);
-         storyFromDatabase.save(function(err) {
-          console.log(storyFromDatabase);
+      req.body.userID = req.user._id;
+      console.log(storyFromDatabase)
+      storyFromDatabase.content.push(req.body);
+      storyFromDatabase.save(function(err) {
+        console.log(storyFromDatabase);
           res.redirect(`/stories/${storyFromDatabase._id}`); 
         });
     });
 });
+
 
 
 router.delete('/:id', function(req, res, next){
@@ -40,29 +33,14 @@ router.delete('/:id', function(req, res, next){
 
 
 
-
-
-
-// router.put('/:id', function(req, res){
-//   Story.findById(req.params.id, function(err, storyFromDatabase){
-//     req.body.userID = req.user._id;
-//     storyFromDatabase.content.findByIdAndUpdate(req.params.id, req.body)
-//     res.redirect(`/stories/${storyFromDatabase._id}`)
-//   })
-// })
+router.put('/:id', function(req, res){
+  Story.findById(req.params.id, function(err, storyFromDatabase){
+    req.body.userID = req.user._id;
+    storyFromDatabase.content.findByIdAndUpdate(req.params.id, req.body)
+    res.redirect(`/stories/${storyFromDatabase._id}`)
+  })
+})
  
-
-
-
-
-
-// router.put('/:id', async function (req, res, next){
-//   console.log('what is going on')
-//   const updatePost = await Story.findByIdAndUpdate(req.params.id, req.body, {new: true});
-//     res.redirect('/stories/' + req.params.id)
-// });
-
-
 
 
 module.exports = router
